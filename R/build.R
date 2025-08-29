2b890aeeea4fb9936a46cdffd75091ef4ce48f81
David JP O'Sullivan <David.OSullivan@ul.ie>
2025-08-29 19:04:12 +0100

    Update build.R

--- R/build.R
+++ R/build.R
@@ -34,31 +34,32 @@ create_timetable_csv <- function(output_file = "data/AY25-26_sem_1_timetable.csv
 # current 
 output_file = "data/AY25-26_sem_1_timetable.csv" 
 excel_sheet = "AY2025-2026 Sem 1"
-excel_path = "data/seminars.xlsx"
+excel_path = "data/Seminars.xlsx"
 create_timetable_csv(output_file, excel_sheet, excel_path)
 
 # next sem open slots
 output_file = "data/AY25-26_sem_2_timetable.csv" 
 excel_sheet = "AY2025-2026 Sem 2"
-excel_path = "data/seminars.xlsx"
+excel_path = "data/Seminars.xlsx"
 create_timetable_csv(output_file, excel_sheet, excel_path)
 
 # older sems --------------------------------------------------------------
 
 output_file = "data/AY24-25_sem_2_timetable.csv"
 excel_sheet = "AY2024-2025 Sem 2"
-excel_path = "data/seminars.xlsx"
+excel_path = "data/Seminars.xlsx"
 create_timetable_csv(output_file, excel_sheet, excel_path)
 
 output_file = "data/AY24-25_sem_1_timetable.csv"
 excel_sheet = "AY2024-2025 Sem 1"
-excel_path = "data/seminars.xlsx"
+excel_path = "data/Seminars.xlsx"
 create_timetable_csv(output_file, excel_sheet, excel_path)
 
 output_file = "data/AY23-24_sem_2_timetable.csv"
 excel_sheet = "AY2023-2024 Sem 2"
-excel_path = "data/seminars.xlsx"
+excel_path = "data/Seminars.xlsx"
 create_timetable_csv(output_file, excel_sheet, excel_path)
+
 
 
 
